b28eb3635efcfda3806fae14087774b90912079b
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-15 20:31:14 -0500

    funcion calcular integrada en simulador.js, lo que mostrara en pantalla los ingresos y egresos pasando por calcularDisponible()

--- simulador.js
+++ simulador.js
@@ -1,1 +1,7 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+  let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+  let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
+  let disponible = calcularDisponible(ingresos, egresos);
+  document.getElementById("lblDisponibleValor").textContent = disponible;
+}
