987145ef8210e0412318610d71caf9fbd2bb2019
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-15 20:41:28 -0500

    funcion de calcularCapacidadPago integrada en funciones.js

--- funciones.js
+++ funciones.js
@@ -1,3 +1,7 @@
 function calcularDisponible(ingresos, egresos) {
   return (ingresos - egresos) <= 0 ? 0 : ingresos - egresos;
 }
+
+function calcularCapacidadPago(montoDisponible) {
+  return montoDisponible * 0.5;
+}
