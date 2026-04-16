f0e070bfcb83e97373d2b5697086eebac2ad3ff7
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-15 20:46:06 -0500

    calculamos la capacidad de pago de lo disponible y se muetra en pantalla

--- simulador.js
+++ simulador.js
@@ -3,5 +3,8 @@ function calcular() {
   let egresos = parseFloat(document.getElementById("txtEgresos").value);
 
   let disponible = calcularDisponible(ingresos, egresos);
+  let capacidad = calcularCapacidadPago(disponible);
+
   document.getElementById("lblDisponibleValor").textContent = disponible;
+  document.getElementById("lblCapacidadValor").textContent = capacidad;
 }
